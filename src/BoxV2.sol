3b6709aa114d88a7489108592cfd214de52263ca
abgnv <bogunovaleksandar@gmail.com>
2024-12-25 17:00:48 +0200

    upgradeable implementation contracts

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,21 +1,26 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
-    
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
     function initialize() public initializer {
-               __Ownable_init(msg.sender);
+        __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
+    }
+
+    function setNumber(uint256 _number) external {
+        number = _number;
     }
 
     function getNumber() external view returns (uint256) {
@@ -23,7 +28,8 @@ contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     }
 
     function version() external pure returns (uint256) {
-        return 1;
+        return 2;
     }
-    function _authorizeUpgrade(address newImplementation) internal override{}
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
